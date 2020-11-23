9177031693d2380548f76c94c2ef7659b4b68a81
erikorange <erikorange@gmail.com>
2020-11-23 12:13:40 -0500

    added button disabling

--- button.py
+++ button.py
@@ -11,23 +11,28 @@ class Button():
         self.__font = font
         self.__btnColorOff = pygame.Color(btnColor)
         self.__btnColorOn = pygame.Color(btnColor).correct_gamma(0.25)
+        self.__btnColorDisabled = pygame.Color((5,5,5))
         self.__textColorOff = pygame.Color(textColor)
         self.__textColorOn = pygame.Color(textColor).correct_gamma(0.25)
         self.__text = text
         self.__onCallback = onCallback
         self.__offCallback = offCallback
-        
         self.__state = False
+        self.disabled = False
         self.drawButton(self.__state)
 
     def drawButton(self, state):
+        self.disabled = False
         if (state):
-            bgColor = self.__btnColorOn
-            txtColor = self.__textColorOn
+            self.__renderButton(self.__btnColorOn, self.__textColorOn)
         else:
-            bgColor = self.__btnColorOff
-            txtColor = self.__textColorOff
+            self.__renderButton(self.__btnColorOff, self.__textColorOff)
 
+    def disableButton(self):
+        self.disabled = True
+        self.__renderButton(self.__btnColorDisabled, self.__textColorOff)
+
+    def __renderButton(self, bgColor, txtColor):
         self.__buttonRect = pygame.draw.rect(self.__screen, bgColor, (self.__posX, self.__posY, self.__sizeX, self.__sizeY), border_radius=10)
         txt = self.__font.render(self.__text, 1, txtColor)
         txtCenterX = self.__posX + (self.__sizeX/2)
